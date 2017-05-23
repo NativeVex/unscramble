f2ce3ac5247a9e518be047a5d22a2958761d9676
Cloud User <ec2-user@ip-172-31-4-122.us-west-2.compute.internal>
2017-05-23 10:17:08 -0400

    jenny's first commit

--- unscramble.cpp
+++ unscramble.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 char Unscramble() {
-return ' ';
+
+return 'c';
 }
 
 void UpdateAI() {
@@ -11,9 +12,27 @@ void UpdateAI() {
 }
 
 int main() {
-char userInput[1024];
-cout << "Enter a five word SCRAMBLED sentence: " << endl;
-cin >> userInput;
-cout << userInput << endl;
+char w1[32];
+char w2[32];
+char w3[32];
+char w4[32];
+char w5[32];
+
+cout << "Enter a the first word in the SCRAMBLED sentence: " << endl;
+cin >> w1;
+cout << "Second word: " << endl;
+cin >> w2;
+cout << "Third word: " << endl;
+cin >> w3;
+cout << "Fourth word: " << endl;
+cin >> w4;
+cout << "Fifth word: " << endl;
+cin >> w5;
+
+//debug
+//cout << "The sentence you have entered is: " << w1 << w2 << w3 << w4 << w5 << endl;
+
+cout << Unscramble();
+
 return 0;
 }
