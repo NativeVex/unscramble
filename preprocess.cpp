d34c9f881d097c384ace622bfc0bae91d4c73d5b
Cloud User <ec2-user@ip-172-31-4-122.us-west-2.compute.internal>
2017-06-07 09:41:26 -0400

    preprocessed script`

---
+++ preprocess.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+cout << "This ran"<< endl;
+
+}
+
+void NumLetters(){
+char letters[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+
+cout << "This ran" << endl;
+}
